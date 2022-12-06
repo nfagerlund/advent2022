90074d215f730ffd26f0f121bc7935c60142137d
Nick Fagerlund <nick.fagerlund@gmail.com>
2022-12-05 17:55:59 -0800

    day 2 part 2... shoulda translated to enums from the start!

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -4,10 +4,78 @@ use std::collections::HashMap;
 fn main() {
     let inputs = load_inputs("day2").unwrap();
     println!("total score after playing all rounds: {}", part_one(&inputs));
-    // println!("two: {}", part_two(&inputs));
+    println!("two: {}", part_two(&inputs));
 }
 
-fn part_two(inputs: &str) -> () {}
+fn real_outcome(code: &str) -> Outcome {
+    match code {
+        "X" => Outcome::Lose,
+        "Y" => Outcome::Draw,
+        "Z" => Outcome::Win,
+        _ => panic!("bye"),
+    }
+}
+
+fn real_faced_shape(code: &str) -> Shape {
+    match code {
+        "A" => Shape::Rock,
+        "B" => Shape::Paper,
+        "C" => Shape::Scissors,
+        _ => panic!("meh"),
+    }
+}
+
+enum Shape {
+    Rock,
+    Scissors,
+    Paper,
+}
+
+enum Outcome {
+    Win,
+    Lose,
+    Draw,
+}
+
+fn part_two(inputs: &str) -> usize {
+    // Ok, exact same idea, but.
+    let mut scoring: HashMap<String, usize> = HashMap::new();
+    for &outcome_code in ["X", "Y", "Z"].iter() {
+        // x => lose, y => draw, z => win
+        for &face_code in ["A", "B", "C"].iter() {
+            let match_text = format!("{face_code} {outcome_code}");
+            let result_score: usize = match real_outcome(outcome_code) {
+                Outcome::Lose => 0,
+                Outcome::Draw => 3,
+                Outcome::Win => 6,
+            };
+            let desired_shape = match (real_faced_shape(face_code), real_outcome(outcome_code)) {
+                (Shape::Paper, Outcome::Win) => Shape::Scissors,
+                (Shape::Paper, Outcome::Lose) => Shape::Rock,
+                (Shape::Paper, Outcome::Draw) => Shape::Paper,
+
+                (Shape::Rock, Outcome::Win) => Shape::Paper,
+                (Shape::Rock, Outcome::Lose) => Shape::Scissors,
+                (Shape::Rock, Outcome::Draw) => Shape::Rock,
+
+                (Shape::Scissors, Outcome::Win) => Shape::Rock,
+                (Shape::Scissors, Outcome::Lose) => Shape::Paper,
+                (Shape::Scissors, Outcome::Draw) => Shape::Scissors,
+            };
+            let shape_score: usize = match desired_shape {
+                Shape::Rock => 1,
+                Shape::Paper => 2,
+                Shape::Scissors => 3,
+            };
+            let score = shape_score + result_score;
+            scoring.insert(match_text, score);
+        }
+    }
+
+    inputs.lines()
+        .map(|match_text| scoring.get(match_text).unwrap())
+        .fold(0, |total, round| total + *round)
+}
 
 // a, x = rock (1 point)
 // b, y = paper (2 points)
@@ -83,7 +151,7 @@ C Z
 
     #[test]
     fn example_part_two() {
-        let answer = ();
+        let answer = 12;
         let result = part_two(EXAMPLE);
         assert_eq!(result, answer);
     }
