171f7db63c115ef34001d77be19f82b89f24dede
Nick Fagerlund <nick.fagerlund@gmail.com>
2022-12-07 13:59:53 -0800

    day 3 part 1

---
+++ src/bin/day3.rs
@@ -0,0 +1,134 @@
+use std::collections::{HashMap, HashSet};
+
+use advent2022::*;
+
+fn main() {
+    let inputs = load_inputs("day3").unwrap();
+    println!("one: {}", part_one(&inputs));
+    println!("two: {}", part_two(&inputs));
+}
+
+fn build_priorities() -> HashMap<char, usize> {
+    let mut stuff = HashMap::new();
+    for (i, l) in ('a'..='z').enumerate() {
+        stuff.insert(l, i + 1);
+    }
+    for (i, l) in ('A'..='Z').enumerate() {
+        stuff.insert(l, i + 27);
+    }
+    stuff
+}
+
+struct Priorities;
+impl Priorities {
+    fn get(c: char) -> Option<usize> {
+        if let Ok(n) = u8::try_from(c) {
+            Priorities::get_byte(n)
+        } else {
+            None
+        }
+    }
+
+    fn get_byte(b: u8) -> Option<usize> {
+        let n = b as usize;
+        if n > 96 && n < 123 {
+            // lowercase a = 97
+            Some(n - 96)
+        } else if n > 64 && n < 91 {
+            // capital A = 65
+            Some(n - 38)
+        } else {
+            None
+        }
+    }
+}
+
+fn part_two(inputs: &str) -> usize {
+
+    0
+}
+
+fn part_one(inputs: &str) -> usize {
+    inputs.lines().fold(0_usize, |sum, line| {
+        let bytes = line.as_bytes();
+        let len = bytes.len();
+        let half = len / 2;
+        let (first, second) = bytes.split_at(half);
+        let mut seen: HashSet<u8> = HashSet::new();
+        for &i in first.iter() {
+            seen.insert(i);
+        }
+        let repeated = second.iter().find(|&i| { seen.contains(i) }).unwrap();
+        let priority = Priorities::get_byte(*repeated).unwrap();
+        sum + priority
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const EXAMPLE: &str = r#"vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+"#;
+
+    #[test]
+    fn example_part_one() {
+        let answer = 157;
+        let result = part_one(EXAMPLE);
+        assert_eq!(result, answer);
+    }
+
+    // #[test]
+    // fn example_part_two() {
+    //     let answer = ();
+    //     let result = part_two(EXAMPLE);
+    //     assert_eq!(result, answer);
+    // }
+
+    #[test]
+    fn dumb() {
+        for i in ('a'..='z').enumerate() {
+            dbg!(&i);
+        }
+    }
+
+    #[test]
+    fn priorities() {
+        let stuff = build_priorities();
+        assert_eq!(*(stuff.get(&'z').unwrap()), 26);
+        assert_eq!(*(stuff.get(&'Y').unwrap()), 51);
+    }
+
+    #[test]
+    fn alt_priorities() {
+        assert_eq!(Priorities::get('z').unwrap(), 26);
+        assert_eq!(Priorities::get('Y').unwrap(), 51);
+    }
+
+    fn chardump(c: char) {
+        let byte: u8 = c.try_into().unwrap();
+        dbg!(&c, &byte);
+    }
+
+    #[test]
+    fn dumber() {
+        for &c in ['a', 'z', 'A', 'Z'].iter() {
+            chardump(c);
+        }
+    }
+
+    #[test]
+    fn half() {
+        let stuff = "aoeuhtns";
+        let bytes = stuff.as_bytes();
+        let len = bytes.len();
+        let half = len / 2;
+        let second_half = bytes.split_at(half).1;
+        let first_char: char = second_half[0].into();
+        assert_eq!(first_char, 'h');
+    }
+}
